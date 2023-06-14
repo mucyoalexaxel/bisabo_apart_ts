98908a3e05b11c9bdb312168ae77e5e71c17bcf5
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-06-14 10:06:23 +0200

    remove pnpm

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import AboutUs8 from "../components/About-Us8";
-import MainLayout from "../layouts/main";
+import React from 'react';
+import AboutUs8 from '../components/About-Us8';
+import MainLayout from '../layouts/main';
 // import Services7 from "../components/Services7";
-import Portfolio2 from "../components/Portfolio2";
-import Portfolio3 from "../components/Portfolio3";
-import VideoWithTestimonials from "../components/Video-with-testimonials";
+import Portfolio2 from '../components/Portfolio2';
+import Portfolio3 from '../components/Portfolio3';
+import VideoWithTestimonials from '../components/Video-with-testimonials';
 
-import IntroWithVertical2 from "../components/Intro-with-vertical2";
-import WorkWithoutFilter from "../components/Work-Without-Filter";
+import IntroWithVertical2 from '../components/Intro-with-vertical2';
+import WorkWithoutFilter from '../components/Work-Without-Filter';
 
 const Home7 = () => {
   React.useEffect(() => {
-    document.querySelector("body").classList.add("index3");
+    document.querySelector('body').classList.add('index3');
   });
   return (
     <MainLayout>
@@ -20,7 +20,7 @@ const Home7 = () => {
       {/* <Services7 /> */}
       <Portfolio2 />
       <Portfolio3 />
-      <VideoWithTestimonials />
+      {/* <VideoWithTestimonials /> */}
     </MainLayout>
   );
 };
