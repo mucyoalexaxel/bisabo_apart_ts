ebeec25483c3a717f34344d398abaa21e4568fc3
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-06-14 12:24:13 +0200

    update contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import PageHeader from "../../components/Page-header";
-import ContactInfo from "../../components/Contact-info";
-import ContactWithMap from "../../components/Contact-with-map";
-import MainLayout from "../../layouts/main";
+import React from 'react';
+import PageHeader from '../../components/Page-header';
+import ContactInfo from '../../components/Contact-info';
+import ContactWithMap from '../../components/Contact-with-map';
+import MainLayout from '../../layouts/main';
 
 const Contact = () => {
-   React.useEffect(() => {
-     document.querySelector("body").classList.add("index3");
-   }, []);
+  React.useEffect(() => {
+    document.querySelector('body').classList.add('index3');
+  }, []);
   return (
     <MainLayout>
       <PageHeader
         title="Contact Us"
         fullPath={[
-          { id: 1, name: "home", url: "/" },
-          { id: 2, name: "contact us", url: "/contact" },
+          { id: 1, name: 'home', url: '/' },
+          { id: 2, name: 'contact us', url: '/contact' },
         ]}
-        image="/assets/img/pg2.jpg"
+        image="/assets/images/night-pool-4.jpeg"
       />
       <section className="contact">
         <ContactInfo />
