c750ce063ac2a0d2b9dd3f7ea2d34a3dc7ecf963
Yannick M <85898240+mugishayannick@users.noreply.github.com>
2023-11-28 14:57:12 +0200

    Fixed some changes

--- src/components/About-Us8/index.jsx
+++ src/components/About-Us8/index.jsx
@@ -1,57 +1,54 @@
 /* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
 import React from 'react';
 
-const AboutUs8 = () => {
-  return (
-    <section className="about section-padding">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-5 exp">
-            <div className="mb-50">
-              <h6 className="fw-100 text-u ls10 main-color mb-10">About Us</h6>
-              <h3 className="fw-400 text-u ls4">Our Company</h3>
+const
+  AboutUs8 = () => {
+    return (
+      <section className="about section-padding">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-5 exp">
+              <div className="mb-50">
+                <h3 className="fw-400 text-u ls4">Bisabo Suites</h3>
+              </div>
+              <div className="box">
+                <h2 className="fz-30 fw-600 mb-20">
+                  Discover Kigali&apos;s Oasis
+                </h2>
+                <p>
+                  Welcome to Bisabo Suites, your exclusive oasis in the heart of Kigali&apos;s vibrant Kimihurura neighborhood. Nestled in a residential area, our premier apartments provide a tranquil retreat amidst the bustling city life. Imagine strolling through tree-lined streets and exploring the local charm, all within reach of supermarkets, restaurants, and essential shops.
+                </p>
+              </div>
             </div>
-            <div className="box">
-              <h2 className="fz-40 fw-600 ls1 mb-20">
-                Where Luxury <br />
-                Meets Lifestyle.
-              </h2>
-              <p>
-                We are a leading provider of luxury apartment accommodations,
-                offering a remarkable blend of style, comfort, and personalized
-                service for discerning guests seeking a refined living
-                experience.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-7 img">
-            <img src="/assets/images/logo.jpeg" alt="" />
+            <div className="col-lg-7 img">
+              <Image layout='fill' src="/assets/images/BisabosuitesLogo.png" alt="" />
 
-            <div className="feat mt-30">
-              <div className="item">
-                <div>
-                  <h5>01</h5>
-                  <h6>Excellence</h6>
+              <div className="feat mt-30">
+                <div className="item">
+                  <div>
+                    <h5>01</h5>
+                    <h6>Excellence</h6>
+                  </div>
                 </div>
-              </div>
-              <div className="item">
-                <div>
-                  <h5>02</h5>
-                  <h6>Serene</h6>
+                <div className="item">
+                  <div>
+                    <h5>02</h5>
+                    <h6>Serene</h6>
+                  </div>
                 </div>
-              </div>
-              <div className="item">
-                <div>
-                  <h5>03</h5>
-                  <h6>Innovative</h6>
+                <div className="item">
+                  <div>
+                    <h5>03</h5>
+                    <h6>Innovative</h6>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  };
 
 export default AboutUs8;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import AboutUs8 from '../components/About-Us8';
 import MainLayout from '../layouts/main';
-// import Services7 from "../components/Services7";
 import Portfolio2 from '../components/Portfolio2';
 import Portfolio3 from '../components/Portfolio3';
-import VideoWithTestimonials from '../components/Video-with-testimonials';
-
 import IntroWithVertical2 from '../components/Intro-with-vertical2';
-import WorkWithoutFilter from '../components/Work-Without-Filter';
 
 const Home7 = () => {
   React.useEffect(() => {
@@ -17,10 +13,8 @@ const Home7 = () => {
     <MainLayout>
       <IntroWithVertical2 />
       <AboutUs8 />
-      {/* <Services7 /> */}
       <Portfolio2 />
       <Portfolio3 />
-      {/* <VideoWithTestimonials /> */}
     </MainLayout>
   );
 };
